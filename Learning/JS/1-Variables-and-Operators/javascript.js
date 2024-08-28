997b863c916900e110191e9cc6c99ae5e530ae4f
ragingbeastz <dimithriweera@gmail.com>
2024-08-28 11:22:26 +0200

    chore: Add JavaScript files and update HTML and CSS files for Variables and Operators lesson

--- Learning/JS/1-Variables-and-Operators/javascript.js
+++ Learning/JS/1-Variables-and-Operators/javascript.js
@@ -1,5 +1,27 @@
+// Assigning Variables
 let name = "John";
 let surname = "Doe";
 
+//Outputting Variables
 console.log(name);
 console.log(surname);
+
+//Reassigning Variables
+let age = 11;
+console.log(age);
+
+age = 54;
+ 
+console.log(age);
+
+//Establishing Constants
+const pi = 3.14;
+console.log(pi);
+
+//Var - deprecated method
+var thisVar = "This is an old deprecated variable";
+console.log(thisVar);
+
+//Mathematics
+let x = (3 + 2) - 76 * (1 + 1);
+console.log(x);
