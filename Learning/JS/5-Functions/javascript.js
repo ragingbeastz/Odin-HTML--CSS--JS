327e1e051bdae9392f037f7b91e4785a9f8640fd
ragingbeastz <dimithriweera@gmail.com>
2024-08-30 20:17:34 +0200

    Revert "Delete unnecessary files and update JavaScript and CSS files"
    This reverts commit 5e4d0230542f5bab4082214affd6b9047fb0c8d8.

--- Learning/JS/5-Functions/javascript.js
+++ Learning/JS/5-Functions/javascript.js
@@ -3,30 +3,3 @@ function favoriteAnimal(animal) {
 }
 
 console.log(favoriteAnimal('Goat'))
-
-//Anonymous Functions
-//e.g. When running code once someone types into a text box
-// you must use addListener() function which uses
-//
-// - the name of the event to listen for e.g. 'keydown'
-// - the function to run when the event occurs
-//i.e.
-
-function logKey(event) {
-    console.log(`You pressed "${event.key}".`);
-  }
-  
-  textBox.addEventListener("keydown", logKey);
-
-  //Or use an anonymous function which does not need a name declaration
-
-textBox.addEventListener("keydown", 
-   function (event) {
-   console.log(`You pressed "${event.key}".`);}
-);
-
-//Arrow Functions
-//Same principle, uses arrow only
-textBox.addEventListener("keydown", (event) => {
-    console.log(`You pressed "${event.key}".`);
-  });
